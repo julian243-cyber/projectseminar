803720919f400e290b47cf13c0b8c9f15e0c1804
Cronkhinator <cronkhinator.coc@gmail.com>
2025-06-01 20:18:51 +0200

    Trained new hybrid models and fixed error

--- model/u_net.py
+++ model/u_net.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import (Input, Conv2D, MaxPooling2D, Conv2DTranspose, Cropping2D, concatenate)
+from tensorflow.keras.layers import (Input, Conv2D, MaxPooling2D, Conv2DTranspose, Cropping2D, concatenate, Dense, Reshape)
 from tensorflow.keras.models import Model
 
 
@@ -165,6 +165,57 @@ def unet_model_same_padding(in_channels = input_channels, out_channels = output_
     outputs = Conv2D(out_channels, kernel_size=1, activation="softmax")(c9)     # -> 322×322×4
     
     return Model(inputs=inputs, outputs=outputs, name="U-Net_322x322_same_padding")
+
+def unet_metadata_in_bottleneck(in_channels = input_channels, out_channels = output_channels, metadata_channels = 5):
+    inputs = Input(shape=(image_height, image_width, in_channels), name="input_image")
+    metadata = Input(shape=(metadata_channels,), name="input_metadata")
+
+    #---Encoder---
+    c1 = double_conv(inputs, con_layers_1, "same")      # -> 322x322x64
+    p1 = MaxPooling2D(pool_size = max_pool_size)(c1)    # -> 161x161x64
+
+    c2 = double_conv(p1,con_layers_2, "same")           # -> 161x161x128
+    p2 = MaxPooling2D(pool_size = max_pool_size)(c2)    # -> 80x80x128
+
+    c3 = double_conv(p2, con_layers_3, "same")          # -> 80×80×256
+    p3 = MaxPooling2D(pool_size = max_pool_size)(c3)    # -> 40×40×256
+
+    c4 = double_conv(p3, con_layers_4, "same")          # -> 40×40×512
+    p4 = MaxPooling2D(pool_size = max_pool_size)(c4)    # -> 20×20×512
+    
+    # ---bottleneck---
+    c5 = double_conv(p4, con_layers_5, "same")          # Bottleneck: 20×20×1024
+    
+    # Concatenate metadata with the bottleneck feature map
+    x_meta = Dense(256, activation='relu')(metadata)  # Reduce metadata to match feature map depth
+    x_meta = Dense(20*20*5, activation='relu')(x_meta)  # Flatten to match spatial dimensions
+    x_meta = Reshape((20, 20, 5))(x_meta)  # Reshape to match spatial dimensions
+
+    bottleneck = concatenate([c5, x_meta], axis=-1)  # (20, 20, 1029)
+
+    #---decoder---
+
+    u6 = Conv2DTranspose(512, kernel_size=2, strides=2, padding='same')(bottleneck)     # -> 40×40×512
+    m6   = concatenate([u6, c4], axis=-1)
+    c6   = double_conv(m6, 512, "same")                                         # -> 40×40×512
+
+    u7 = Conv2DTranspose(256, kernel_size=2, strides=2, padding='same')(c6)     # -> 80×80×256
+    m7   = concatenate([u7, c3], axis=-1)
+    c7   = double_conv(m7, 256, "same")                                         # -> 80×80×256
+
+    u8 = Conv2DTranspose(128, kernel_size=2, strides=2, padding='same')(c7)     # -> 160×160×128
+    u8 = tf.keras.layers.ZeroPadding2D(padding=((0, 1), (0, 1)))(u8)            # -> 161×161×128
+    m8   = concatenate([u8, c2], axis=-1)
+    c8   = double_conv(m8, 128, "same")                                         # -> 161×161×128
+
+    u9 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same')(c8)      # -> 322×322×64
+    m9   = concatenate([u9, c1], axis=-1)
+    c9   = double_conv(m9, 64, "same")                                          # -> 322×322×64
+
+    # --- Ausgabe 1×1 Conv ---
+    outputs = Conv2D(out_channels, kernel_size=1, activation="softmax")(c9)     # -> 322×322×4
+
+    return Model(inputs=[inputs, metadata], outputs=outputs, name="U-Net_322x322_same_padding")
 
 
 
